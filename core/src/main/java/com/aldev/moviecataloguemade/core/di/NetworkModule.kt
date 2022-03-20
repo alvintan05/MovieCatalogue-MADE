284fd7213679fb290682e91eb616c938add548af
Alvin Tandiardi <alvintandiardi@gmail.com>
2022-03-20 15:02:13 +0700

    add certificate pinning

--- core/src/main/java/com/aldev/moviecataloguemade/core/di/NetworkModule.kt
+++ core/src/main/java/com/aldev/moviecataloguemade/core/di/NetworkModule.kt
@@ -6,6 +6,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,13 +18,25 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
+    @Singleton
     @Provides
-    fun provideOkHttpClient(): OkHttpClient =
+    fun provideCertificatePinner(): CertificatePinner =
+        CertificatePinner.Builder()
+            .add(BuildConfig.HOST_URL, "sha256/oD/WAoRPvbez1Y2dfYfuo4yujAcYHXdv1Ivb2v2MOKk=")
+            .add(BuildConfig.HOST_URL, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(BuildConfig.HOST_URL, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(BuildConfig.HOST_URL, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(certificatePinner: CertificatePinner): OkHttpClient =
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(60, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
             .writeTimeout(60, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
 
     @Singleton
